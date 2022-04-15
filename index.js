f3ac50dd6c2e56fb4a3c71745bd175a187bce828
Amelia Wibisono <ameliawibi@gmail.com>
2022-04-15 21:36:12 +0800

    postgres

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 import express from 'express';
+import pg from "pg";
+
+const { Pool } = pg;
+
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'heroku',
+    port: 5432,
+  };
+}
+const pool = new Pool(pgConnectionConfigs);
+
 
 const PORT = process.env.PORT || 3004;
 
@@ -16,6 +42,17 @@ app.get('/bananas', (request, response) => {
   const data = { responseText };
 
   response.render('bananas', data);
+});
+
+app.get('/cats', (request, response) => {
+  console.log('request came in');
+  pool.query('SELECT * from cats').then((result) => {
+    console.log(result.rows[0].name);
+    response.send(result.rows);  
+  }).catch((error) => {
+    console.error('Error executing query', error.stack);
+    response.status(503).send(result.rows);
+  });;
 });
 
 app.listen(PORT);
